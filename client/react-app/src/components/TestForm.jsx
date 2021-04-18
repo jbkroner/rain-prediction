e0aa47673034312c31c047d6dab88463418ed98d
sam kersebet <sam.kersebet@gmail.com>
2021-04-17 20:25:23 -0500

    test form up and running on localhost 3000

--- client/react-app/src/components/TestForm.jsx
+++ client/react-app/src/components/TestForm.jsx
@@ -1,16 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+//import ReactDOM from 'react-dom';
 import { useFormik } from 'formik';
 
-class TestForm extends React.Component {
-  render(){
+function TestForm () {
   const formik = useFormik({
     initialValues: {
     Location :'',//select
     MinTemp:'',//numeric
     MaxTemp:'',//num
     Rainfall:'',//num
-    sunshine:'',//num
+    Sunshine:'',//num
     WindGustDir:'', //Select
     WindGustSpeed:'', //numeric
     WindDir9am:'',//Select
@@ -19,7 +18,7 @@ class TestForm extends React.Component {
     Windspeed3pm:'', //num
     Humitidy9am :'', //num
     Humidity3pm :'', //num
-    Pressaure9am:'', //num
+    Pressure9am:'', //num
     Pressure3pm:'', //num
     Cloud9am:'', //num
     Cloud3pm:'', //num
@@ -76,13 +75,162 @@ class TestForm extends React.Component {
         value={formik.values.Rainfall}
       />
       </p>
+      <p>
+      <label htmlFor="Sunshine">Sunshine</label>
+      <input
+        id="Sunshine"
+        name="Sunshine"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Sunshine}
+      />
+      </p>
+      <p>
+      <label htmlFor="Rainfall">WindGustDir</label>
+      <input
+        id="WindGustDir"
+        name="WindGustDir"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.WindGustDir}
+      />
+      </p>
+      <p>
+      <label htmlFor="WindGustSpeed">WindGustSpeed</label>
+      <input
+        id="WindGustSpeed"
+        name="WindGustSpeed"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.WindGustSpeed}
+      />
+      </p>
+      <p>
+      <label htmlFor="WindDir9am">WindDir9am</label>
+      <input
+        id="WindDir9am"
+        name="WindDir9am"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.WindDir9am}
+      />
+      </p>
+      <p>
+      <label htmlFor="WindSpeed9pm">WindSpeed9pm</label>
+      <input
+        id="WindSpeed9pm"
+        name="WindSpeed9pm"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.WindSpeed9pm}
+      />
+      </p>
+      <p>
+      <label htmlFor="WindSpeed9am">WindSpeed9am</label>
+      <input
+        id="WindSpeed9am"
+        name="WindSpeed9am"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.WindSpeed9am}
+      />
+      </p>
+      <p>
+      <label htmlFor="Humitidy9am">Humitidy9am</label>
+      <input
+        id="Humitidy9am"
+        name="Humitidy9am"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Humitidy9am}
+      />
+      </p>
+      <p>
+      <label htmlFor="Humidity3pm">Humidity3pm</label>
+      <input
+        id="Humidity3pm"
+        name="Humidity3pm"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Humidity3pm}
+      />
+      </p>
+      <p>
+      <label htmlFor="Pressure9am">Pressure9am</label>
+      <input
+        id="Pressure9am"
+        name="Pressure9am"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Pressure9am}
+      />
+      </p>
+      <p>
+      <label htmlFor="Pressure3pm">Pressure3pm</label>
+      <input
+        id="Pressure3pm"
+        name="Pressure3pm"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Pressure3pm}
+      />
+      </p>
+      <p>
+      <label htmlFor="Cloud9am">Cloud9am</label>
+      <input
+        id="Cloud9am"
+        name="Cloud9am"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Cloud9am}
+      />
+      </p>
+      <p>
+      <label htmlFor="Cloud3pm">Cloud3pm</label>
+      <input
+        id="Cloud3pm"
+        name="Cloud3pm"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Cloud3pm}
+      />
+      </p>
+      <p>
+      <label htmlFor="Temp9am">Temp9am</label>
+      <input
+        id="Temp9am"
+        name="Temp9am"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Temp9am}
+      />
+      </p>
+      <p>
+      <label htmlFor="Temp3pm">Temp3pm</label>
+      <input
+        id="Temp3pm"
+        name="Temp3pm"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.Temp3pm}
+      />
+      </p>
+      <p>
+      <label htmlFor="RainToday">Rain Today</label>
+      <input
+        id="RainToday"
+        name="RainToday"
+        type="text"
+        onChange={formik.handleChange}
+        value={formik.values.RainToday}
+      />
+      </p>
       <button type="submit">Submit</button>
     </form>
     </div>
   );
-  }
-};
+}
+
 
 //Not sure about exporting this to the index.js file
-const element=<TestForm></TestForm>
-export default element;
+export default TestForm;
