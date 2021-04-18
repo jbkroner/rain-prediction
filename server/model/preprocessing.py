b3a3262b4c0f940c41e3a1ce67c7d299e3f718af
jbkroner <jbkroner@gmail.com>
2021-04-17 19:19:08 -0500

    model working

--- server/model/preprocessing.py
+++ server/model/preprocessing.py
@@ -2,11 +2,20 @@
 # pandas
 import pandas as pd
 
+# numpy
+import numpy as np
 
 # scikit
 from sklearn.preprocessing import scale
 from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.compose import ColumnTransformer
+from sklearn.model_selection import train_test_split
+
+import category_encoders as ce
+import warnings
+
+warnings.filterwarnings('ignore')
 
 # load .csv
 print('loading data...', end='')
@@ -15,67 +24,92 @@ print('done!')
 
 # scaling data
 print('scaling data...', end='')
-preData = preData.sample(frac=.10)
+preData = preData.sample(frac=1.0)
 print('done!')
 
-# dropping indepdent columns
-print('dropping indepdent columns...', end='')
+
+# dropping Date columns
+print('dropping date...', end='')
 preData.pop('Date')
 print('done!')
 
-# reformatting columns recorded as string that should be numeric
-print('string to numeric translation...', end='')
-preData['WindDir9am'] = pd.to_numeric(preData['WindDir9am'], errors='coerce')
-preData['WindDir3pm'] = pd.to_numeric(preData['WindDir3pm'], errors='coerce')
+# split training / testing data
+print('splitting training / testing data', end='')
+X = preData.drop(['RainTomorrow'], axis=1)
+y = preData.pop('RainTomorrow')
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+print('done!')
+
+# feature engineering missing numerical data
+print('feature engineering missing numerical data...',end='')
+numerical = [col for col in X_train.columns if X_train[col].dtypes != 'O']
+for df1 in [X_train, X_test]:
+    for col in numerical:
+        col_median=X_train[col].median()
+        df1[col].fillna(col_median, inplace=True)
 print('done')
 
+# feature engineering missing catergorical data
+print('feature engineering missing catergorical data...', end="")
+for df2 in [X_train, X_test]:
+    df2['WindGustDir'].fillna(X_train['WindGustDir'].mode()[0], inplace=True)
+    df2['WindDir9am'].fillna(X_train['WindDir9am'].mode()[0], inplace=True)
+    df2['WindDir3pm'].fillna(X_train['WindDir3pm'].mode()[0], inplace=True)
+    df2['RainToday'].fillna(X_train['RainToday'].mode()[0], inplace=True)
 
-# separate target
-print('separating target...', end='')
-y = preData.pop('RainTomorrow')
+print('done')
+
+print('feature engineer: remove samples with no label', end='')
+# guessing on missing features, this is probably bad
+for sample in [y_train, y_test]:
+    sample.fillna(y_train.mode()[0], inplace=True)
+print('done')
+
+# engineering numerical outliers
+print('feature engineering: numerical outliers...',end='')
+def max_value(df3, variable, top):
+    return np.where(df3[variable]>top, top, df3[variable])
+
+for df3 in [X_train, X_test]:
+    df3['Rainfall'] = max_value(df3, 'Rainfall', 3.2)
+    df3['Evaporation'] = max_value(df3, 'Evaporation', 21.8)
+    df3['WindSpeed9am'] = max_value(df3, 'WindSpeed9am', 55)
+    df3['WindSpeed3pm'] = max_value(df3, 'WindSpeed3pm', 57)
 print('done!')
 
-# handle missing data:  replace with mean data
-print('handling missing data...', end="")
-for feature in preData:
-    if preData[feature].dtype==float:
-        m = preData[feature].mean()
-        preData[feature].fillna(m, inplace=True)
-    else:
-        mo = preData[feature].mode()[0]
-        preData[feature].fillna(mo, inplace=True)
+print('feature engineering: nominal data encoding...', end='')
+encoder = ce.BinaryEncoder(cols=['RainToday'])
+X_train = encoder.fit_transform(X_train)
+X_test = encoder.transform(X_test)
+
+X_train = pd.concat([X_train[numerical], X_train[['RainToday_0', 'RainToday_1']],
+                     pd.get_dummies(X_train.Location), 
+                     pd.get_dummies(X_train.WindGustDir),
+                     pd.get_dummies(X_train.WindDir9am),
+                     pd.get_dummies(X_train.WindDir3pm)], axis=1)
+
+X_test = pd.concat([X_test[numerical], X_test[['RainToday_0', 'RainToday_1']],
+                     pd.get_dummies(X_test.Location), 
+                     pd.get_dummies(X_test.WindGustDir),
+                     pd.get_dummies(X_test.WindDir9am),
+                     pd.get_dummies(X_test.WindDir3pm)], axis=1)
 print('done!')
 
-# encode nominal features
-# handling catergorial features
-print('handling catergorial features...', end='')
-nominalData = [] # append indeces here
+print('feature engineering: scaling...', end="")
+cols = X_train.columns
+scaler = MinMaxScaler()
+X_train = scaler.fit_transform(X_train)
+X_test = scaler.transform(X_test)
 
-for i in range(len(preData.dtypes)):
-    if preData.dtypes[i]==object:
-#        print('------(+) ' + str(preData.columns[i]))
-        nominalData.append(i)
-# print(' ' + str(len(nominalData)) + ' features found!') 
-
-# build the ct
-ct = ColumnTransformer([('e', OneHotEncoder(handle_unknown='ignore'), nominalData)], remainder='passthrough')
-ohe = ct.fit_transform(preData)
-X = pd.DataFrame.sparse.from_spmatrix(ohe, columns = ct.get_feature_names(), index = preData.index)
+X_train = pd.DataFrame(X_train, columns=[cols])
+X_test = pd.DataFrame(X_test, columns=[cols])
 print('done!')
-
 
 print('saving cleaned data...', end="")
-X.to_csv('data-cleaned-X.csv')
-y.to_csv('data-cleaned-y.csv')
+X_train.to_csv('X_train_lite.csv')
+X_test.to_csv('X_test_lite.csv')
+y_train.to_csv('y_train_lite.csv')
+y_test.to_csv('y_test_lite.csv')
 print('done!')
-
-
-
-
-
-
-
-
-
-
 
--- server/model/train.py
+++ server/model/train.py
@@ -1,29 +1,26 @@
-
-
-
 import pandas as pd
 
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import cross_val_score
+from sklearn.metrics import accuracy_score
 
 from joblib import dump, load
 
-X = pd.read_csv('./data-cleaned-X.csv')
-y = pd.read_csv('./data-cleaned-y.csv')
+X_train = pd.read_csv('./X_train_lite.csv')
+y_train = pd.read_csv('./y_train_lite.csv', index_col=False)
+X_test = pd.read_csv('./X_test_lite.csv')
+y_test = pd.read_csv('./y_test_lite.csv', index_col=False)
+model = LogisticRegression(solver='liblinear', random_state=0, verbose=2)
 
-print('training model!')
-parameters = {'min_samples_leaf':[2, 4, 6, 8, 10, 11, 12, 13, 14, 15, 16, 18, 20], 'n_estimators':[20,40,60,80,100,120,140]}
-rf = RandomForestClassifier()
-parameter_search = GridSearchCV(rf, parameters, cv=10, scoring="roc_auc", refit=True, verbose=2, n_jobs=2)
-parameter_search.fit(X, y)
+print(y_train['RainTomorrow'])
 
-meta = GridSearchCV(rf, parameters, cv=5, scoring="roc_auc", refit=True)
-results = cross_val_score(meta, X, y, cv=10, scoring="roc_auc", verbose=2, n_jobs=2)
-print('done!') 
-
-print(results)
+model.fit(X_train, y_train['RainTomorrow'])
 
 print('pickling model...',end='')
-dump(rf, 'model-trained-1.joblib')
+dump(model, 'model-trained-1.joblib')
 print('done!')
+
+print('testing model...')
+y_pred_test = model.predict(X_test)
+print('Model accuracy score: {0:0.4f}'.format(accuracy_score(y_test['RainTomorrow'], y_pred_test)))
